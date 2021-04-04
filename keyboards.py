3f32ddac8f0d7d712a8dde67e79db0e591400f6d
Lo-k1 <74830256+Lo-k1@users.noreply.github.com>
2021-04-04 20:29:46 +0300

    Add files via upload

---
+++ keyboards.py
@@ -0,0 +1,185 @@
+keyboard = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "0",
+                'label': "&#11036;"
+            },
+            'color': 'primary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "1",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "2",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "3",
+                'label': "&#11036;"
+            },
+            'color': 'primary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "4",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "5",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "6",
+                'label': "&#11036;"
+            },
+            'color': 'primary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "7",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "8",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            }
+        ],
+        [
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "11",
+                    'label': "Назад"
+                }
+            },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "10",
+                    'label': "Играть с начала"
+                }
+            }
+        ]
+
+    ]
+}
+keyboard_start = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "100",
+                'label': "Начать"
+            }
+        }
+        ],
+[{
+            'action': {
+                'type': "callback",
+                'payload': "160",
+                'label': "Правила"
+            }
+        }
+        ]
+
+    ]
+}
+keyboard_kresikilinolik = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "105",
+                'label': "&#10060;"
+            }
+        }
+            ,
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "106",
+                    'label': "&#11093;"
+                }
+            }
+        ]
+
+    ]
+}
+keybord_vibor = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "101",
+                'label': "Против компьютера"
+            }
+        },
+        {
+            'action': {
+                'type': "callback",
+                'payload': "102",
+                'label': "С другом"
+            }
+        }],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "103",
+                'label': "Онлайн"
+            }
+        },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "150",
+                    'label': "&#128008;"
+                }
+            }
+        ],
+[
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "161",
+                    'label': "Список лидеров"
+                }
+            }
+        ]
+
+    ]
+}
