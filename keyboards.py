8cdc86e362d557a188697508e28c1578ba3c424e
Lo-k1 <74830256+Lo-k1@users.noreply.github.com>
2021-04-18 21:17:03 +0300

    Add files via upload

---
+++ keyboards.py
@@ -0,0 +1,478 @@
+keybord_nachalo = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "1000",
+                'label': "Крестики-Нолики"
+            }
+        },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "1001",
+                    'label': "Саймон"
+                }
+            }],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "1002",
+                'label': "Купи слона"
+            }
+        },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "1003",
+                    'label': "&#128008;"
+                }
+            }
+        ]
+    ]
+}
+keyboard_slon = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "open_link",
+                "link": "https://market.yandex.ru/search?text=%D1%81%D0%BB%D0%BE%D0%BD&cvredirect=2&cpa=0&onstock=0&local-offers-first=0",
+                'payload': "10000",
+                'label': "Хорошо"
+            }
+        },
+{
+            'action': {
+                'type': "callback",
+                'payload': "10000",
+                'label': "Назад"
+            }
+        }
+        ]
+
+    ]
+}
+keyboard_cat = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "0",
+                'label': "&#4448;"
+            },
+            'color': 'secondary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "1",
+                'label': "&#4448;"
+            },
+                'color': 'secondary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "2",
+                'label': "&#4448;"
+            },
+                'color': 'secondary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "3",
+                'label': "&#4448;"
+            },
+            'color': 'secondary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "4",
+                'label': "&#4448;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "5",
+                'label': "&#4448;"
+            },
+                'color': 'secondary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "6",
+                'label': "&#128008;"
+            },
+            'color': 'secondary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "7",
+                'label': "&#4448;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "8",
+                'label': "&#128045;"
+            },
+                'color': 'secondary'
+            }
+        ],
+        [
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "11",
+                    'label': "Назад"
+                }
+            },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "10",
+                    'label': "Играть с начала"
+                }
+            }
+        ]
+
+    ]
+}
+keyboard = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "0",
+                'label': "&#11036;"
+            },
+            'color': 'primary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "1",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "2",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "3",
+                'label': "&#11036;"
+            },
+            'color': 'primary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "4",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "5",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "6",
+                'label': "&#11036;"
+            },
+            'color': 'primary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "7",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "8",
+                'label': "&#11036;"
+            },
+                'color': 'primary'
+            }
+        ],
+        [
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "11",
+                    'label': "Назад"
+                }
+            },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "10",
+                    'label': "Играть с начала"
+                }
+            }
+        ]
+
+    ]
+}
+keyboard_start = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "1004",
+                'label': "Начать"
+            }
+        }
+        ]
+
+    ]
+}
+keyboard_kresikilinolik = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "105",
+                'label': "&#10060;"
+            }
+        }
+            ,
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "106",
+                    'label': "&#11093;"
+                }
+            }
+        ]
+
+    ]
+}
+keybord_vibor = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "101",
+                'label': "Против компьютера"
+            }
+        },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "102",
+                    'label': "С другом"
+                }
+            }],
+        [
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "150",
+                    'label': "Правила"
+                }
+            }]
+        , [
+
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "161",
+                    'label': "Список лидеров"
+                }
+            },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "170",
+                    'label': "Назад"
+                }
+            }
+        ]
+
+    ]
+}
+keyboard_saymon = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "0",
+                'label': "&#11036;"
+            },
+            'color': 'secondary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "1",
+                'label': "&#11036;"
+            },
+                'color': 'secondary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "2",
+                'label': "&#11036;"
+            },
+                'color': 'secondary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "3",
+                'label': "&#11036;"
+            },
+            'color': 'secondary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "4",
+                'label': "&#11036;"
+            },
+                'color': 'secondary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "5",
+                'label': "&#11036;"
+            },
+                'color': 'secondary'
+            }
+        ],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "6",
+                'label': "&#11036;"
+            },
+            'color': 'secondary'
+        },
+
+            {'action': {
+                'type': "callback",
+                'payload': "7",
+                'label': "&#11036;"
+            },
+                'color': 'secondary'
+            },
+
+            {'action': {
+                'type': "callback",
+                'payload': "8",
+                'label': "&#11036;"
+            },
+                'color': 'secondary'
+            }
+        ],
+        [
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "10",
+                    'label': "Играть с начала"
+                }
+            },
+{
+                'action': {
+                    'type': "callback",
+                    'payload': "12",
+                    'label': "Список лидеров"
+                }
+            },
+            {
+                'action': {
+                    'type': "callback",
+                    'payload': "11",
+                    'label': "Назад"
+                }
+            }
+        ]
+
+    ]
+}
+keyboard_start_saymon = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "100",
+                'label': "Начать"
+            }
+        }
+        ]
+
+    ]
+}
+
+keybord_vibor_saymon = {
+    "one_time": True,
+    "buttons": [
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "101",
+                'label': "Легко"
+            }
+        }],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "102",
+                'label': "Средне"
+            }
+        }],
+        [{
+            'action': {
+                'type': "callback",
+                'payload': "103",
+                'label': "Сложно"
+            }
+        }
+        ]
+
+    ]
+}
